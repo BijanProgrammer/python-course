beb908419df411b322b67f20be23cab436fba268
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-10 16:57:49 +0430

    [ADD] session01 - concat.py

---
+++ session01/05_concat.py
@@ -0,0 +1,12 @@
+first_name = "Bijan"
+last_name = "Eisapour"
+age = 21
+
+print(first_name + " " + last_name)
+print("My name is " + first_name + " " + last_name + ".")
+print("My name is " + first_name + " " + last_name + ". I am " + str(age) + " years old.")
+print()
+
+print(first_name, last_name)
+print("My name is", first_name, last_name + ".")
+print("My name is", first_name, last_name + ". I am", age, "years old.")
