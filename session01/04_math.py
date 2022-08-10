5d5b48c014f2f48c3b9739b2ea5fa97626f89440
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-10 16:43:05 +0430

    [ADD] session01 - math.py

---
+++ session01/04_math.py
@@ -0,0 +1,45 @@
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+print(2 // 3)
+print(2 % 3)
+print(2 ** 3)
+print()
+
+x = 23
+y = 42
+z = x + y
+print(z)
+print()
+
+n = 23
+m = n * 2
+o = m + 5
+print(n)
+print(m)
+print(o)
+print()
+
+n = 23
+n = n * 2
+n = n + 5
+print(n)
+print()
+
+a = 23
+a = 42
+print(a)
+print()
+
+b = 23
+b = b + 2
+b = b * 3
+print(b)
+print()
+
+b = 23
+b += 2
+b *= 3
+print(b)
+print()
