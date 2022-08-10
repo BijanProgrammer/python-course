af1cf30871217ac9ee5b03073ed1e92b049d913d
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-10 16:26:03 +0430

    [ADD] session01 - variables.py

---
+++ session01/03_variables.py
@@ -0,0 +1,24 @@
+name = "Bijan"
+print(name)
+print(type(name))
+print()
+
+sentence = "My name is Bijan."
+print(sentence)
+print(type(sentence))
+print()
+
+number = 23
+print(number)
+print(type(number))
+print()
+
+float_number = 23.42
+print(float_number)
+print(type(float_number))
+print()
+
+xyz = True
+print(xyz)
+print(type(xyz))
+print()
