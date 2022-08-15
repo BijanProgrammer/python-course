19993e804fa14a8a7593cbb433b1bdd3248e1f03
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-15 15:36:32 +0430

    [ADD] session 02 - strings - escape

---
+++ session02/02_escape.py
@@ -0,0 +1,24 @@
+paragraph = "This is the first line.\nThis is the second line."
+print(paragraph)
+print()
+
+indented_paragraph = "\tThis is the first line.\nThis\tis\tthe\tsecond\tline."
+print(indented_paragraph)
+print()
+
+# 1234    END
+# 12345   END
+# 123456  END
+# 1234567 END
+# 12345678    END
+
+using_double_quotation_inside_string = "He said: \"I am learning python.\""
+print(using_double_quotation_inside_string)
+
+using_double_quotation_inside_single_quotations = 'He said: "I am learning python."'
+print(using_double_quotation_inside_single_quotations)
+print()
+
+escaping_back_slash = "Python Location: C:\\Users\\Bijan\\AppData\\Local\\Programs\\Python\\Python39\\python.exe"
+print(escaping_back_slash)
+print()
