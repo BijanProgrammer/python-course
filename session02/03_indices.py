16c9daac21a65dc57057408d2725dbd5137d9ebe
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-15 15:48:07 +0430

    [ADD] session 02 - strings - indices

---
+++ session02/03_indices.py
@@ -0,0 +1,10 @@
+username = "BijanProgrammer"
+#           0123456789...
+
+print("username[0]", "\t\t", username[0])
+print("username[0:5]", "\t\t", username[0:5])
+print("username[:5]", "\t\t", username[:5])
+print("username[5:15]", "\t\t", username[5:15])
+print("username[5:]", "\t\t", username[5:])
+print("username[:-3]", "\t\t", username[:-3])
+print("username[-10:-3]", "\t", username[-10:-3])
