2a2212e7a10153f81ffd2c7554747b216b12d85a
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-15 16:20:26 +0430

    [UPDATE] session 02 - strings - format

---
+++ session02/05_format.py
@@ -0,0 +1,8 @@
+first_name = "Bijan"
+last_name = "Eisapour"
+age = 21
+
+print("My name is {} {}. I am {} years old.".format(first_name, last_name, age))
+print(f"My name is {first_name} {last_name}. I am {age} years old.")
+
+print("I am {2} years old. My name is {0} {1}.".format(first_name, last_name, age))
