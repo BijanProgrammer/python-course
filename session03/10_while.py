02d0f347b448cc0f8f6a6413de37ee2c25fed685
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-17 17:01:23 +0430

    [ADD] session 03 - while

---
+++ session03/10_while.py
@@ -0,0 +1,10 @@
+lower_bound = int(input("lower bound: "))
+upper_bound = int(input("upper bound: "))
+step = int(input("step: "))
+
+i = lower_bound
+while i <= upper_bound:
+    print(i)
+    i += step
+
+print("Done!")
---
+++ session03/11_while_list.py
@@ -0,0 +1,7 @@
+people = ["Bijan", "Reza", "Ali", "Mahdi", "Mohammad", "Alireza", "Babak"]
+#            0        1      2       3          4          5         6
+
+i = 0
+while i < len(people):
+    print(f"Hello, {people[i]}!")
+    i += 1
