598677568356eb545a1582438eb38bfc30aa0544
BijanProgrammer <bijaneisapour@gmail.com>
2022-08-22 16:02:35 +0430

    [ADD] session 04 - set

---
+++ session04/07_set.py
@@ -0,0 +1,10 @@
+A = {1, 2, 3, 1, 1, 1, 2, 3}
+B = {2, 1, 3}
+
+print(A)
+
+A.add(23)
+print(A)
+
+A.discard(23)
+print(A)
---
+++ session04/08_set_math_operations.py
@@ -0,0 +1,8 @@
+A = {4, 8, 15, 16, 23, 42}
+B = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+C = {1, 3, 5, 7, 9}
+
+print("A.union(B):", A.union(B))
+print("A.intersection(B):", A.intersection(B))
+print("A.difference(B):", A.difference(B))
+print("A.symmetric_difference(B):", A.symmetric_difference(B))
